c9f5ef370999b89518494402594bc92e73b71db1
bencycco <168569357+bencycco@users.noreply.github.com>
2024-05-07 16:18:22 +0100

    Update post-sway-install.sh

--- post-sway-install.sh
+++ post-sway-install.sh
@@ -1,1 +1,8 @@
+xbps-install xdg-utils rtkit xdg-desktop-portal xdg-user-dirs
 
+xdg-user-dirs-update
+ln -s /etc/sv/rtkit /var/service
+echo "Enter (under new line): "QT_QPA_PLATFORM=wayland-egl""
+echo "and also "SDL_VIDEODRIVER=wayland""
+echo "and also "MOZ_ENABLE_WAYLAND=1""
+echo "and also "ELM_DISPLAY=wl""
