619000e469d0446ab5ff81d51f457bc2396a8321
bencycco <168569357+bencycco@users.noreply.github.com>
2024-05-07 16:20:56 +0100

    Update post-sway-install.sh

--- post-sway-install.sh
+++ post-sway-install.sh
@@ -6,3 +6,8 @@ echo "Enter (under new line): "QT_QPA_PLATFORM=wayland-egl""
 echo "and also "SDL_VIDEODRIVER=wayland""
 echo "and also "MOZ_ENABLE_WAYLAND=1""
 echo "and also "ELM_DISPLAY=wl""
+echo "Press any key and then enter to continue."
+read WAITING
+
+xbps-install qt6-wayland qt6-wayland-devel
+echo "-----ALL DONE-----"
