b0a0d3fbddb3da075237011302ebef0f84f63abb
bencycco <168569357+bencycco@users.noreply.github.com>
2024-05-07 16:12:51 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -6,8 +6,18 @@ xbps-install open-vm-tools
 
 # Enable services
 ln -s /etc/sv/NetworkManager /var/service
-ln -s /etc/sv/elogind /var/service
 ln -s /etc/sv/dbus /var/service
-ln -s /etc/sv/polkitd
-sv up dbus
-sv up elogind
+ln -s /etc/sv/polkitd /var/service
+ln -s /etc/sv/vm* /var/service
+
+xbps-install mesa-dri mesa-vmwgfx-dri
+xbps-install xf86-video-vmware xorg-fonts
+xbps-install wayland wlroots sway dmenu dmenu-wayland rxvt-unicode foot firefox konsole
+
+echo "vmwgfx cursor broken my default."
+echo "Input: "WLR_NO_HARDWARE_CURSORS=1", to /etc/environment"
+echo "Press any key and then enter to continue."
+vim /etc/environment
+echo "Now enter: "dbus-launch sway" to launch sway (after reboot)"
+
+reboot
